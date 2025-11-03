a129cd6c7249c9e653a73896e072fe0537461272
Maged Groups <magedyaseengroups@gmail.com>
2025-11-03 22:14:18 +0300

    feat: Faker PHP added to the project

--- index.php
+++ index.php
@@ -7,6 +7,34 @@ require_once 'bootstrap/app.php';
 
 $employees = App\Models\Employee::all();
 
-dd($employees);
+dump($employees);
 
 echo 'I had the Employees';
+
+$faker = Faker\Factory::create();
+
+dump($faker);
+
+$db = new \mysqli("localhost", "root", "", "classicmodels");
+
+for ($i = 0; $i < 50; $i++) {
+    $employeeNumber = $faker->randomNumber(3, true);
+    $lastName = $faker->firstName;
+    $firstName = $faker->lastName;
+    $extension = $faker->randomNumber(4, true);
+    $email = $faker->safeEmail();
+    $officeCode = $faker->numberBetween(1, 7);
+    $reportsTo = 1102;
+    $jobTitle = $faker->randomElement([
+        'Sales Rep',
+        'Sales Manager',
+        'Area Manager',
+        'Distrect Manager',
+    ]);
+
+    $qry = "INSERT INTO `employees` (`employeeNumber`, `lastName`, `firstName`, `extension`, `email`, `officeCode`, `reportsTo`, `jobTitle`) VALUES('$employeeNumber', '$lastName', '$firstName', '$extension', '$email', '$officeCode', '$reportsTo', '$jobTitle');";
+
+    // dd($qry);
+    $db->query($qry);
+
+}
